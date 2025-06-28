38d7acd7aaeba8308f6dec74d5ecd092cba5050a
illity <arthursbdantas@gmail.com>
2025-06-28 00:06:56 -0300

    docker adjust, test signup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
 
-mongoose.connect('mongodb://localhost:27017/oauth', {}).then(() => {
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/oauth'
+
+
+mongoose.connect(mongoUrl, {}).then(() => {
   console.log('MongoDB connected');
 }).catch(err => {
   console.error('MongoDB connection error:', err);
@@ -15,6 +21,15 @@ type Response = express.Response;
 import { Router } from 'express';
 import authRouter from './authRoutes'; 
 const route = Router()
+
+
+
+app.use(cors({
+  origin: '*',
+  methods: ['POST'],
+  allowedHeaders: ['Content-Type', 'User-Agent'],
+}));
+
 
 app.use(express.json())
 
@@ -32,4 +47,5 @@ app.use(authRouter)
 
 app.listen(3333, () => {
   console.log('server running on port 3333')
+  console.log('yo soy', mongoUrl)
 })
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-    npm install --save-dev nodemon ts-node typescript
+docker-compose up --build
+python -m http.server 5555
+C:\Users\-\Downloads\loophole-cli_1.0.0-beta.15_windows_64bit\loophole.exe http 3333 --hostname oaauth
+C:\Users-\Downloads\loophole-cli_1.0.0-beta.15_windows_64bit\loophole.exe http 5555--hostname frontoaauth
